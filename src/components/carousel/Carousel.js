cef27bc75d6acb4e34c7ae38c3cda257fde31d67
Emanuel Pais <emapais228@gmail.com>
2023-08-31 15:01:28 -0300

    Estructura basica
    Cree la estructura basica para el carousel

--- src/components/carousel/Carousel.js
+++ src/components/carousel/Carousel.js
@@ -10,26 +10,27 @@ const Carousel = () => {
 
     return (
         <section>
-            <div>
-                <div>
+            <div className="flex justify-between ">
+                <div className="bg-gray-400">
                     <img></img>
-                    <h3>titulo 1</h3>
+                    <h3 className="text-black">titulo 1</h3>
                 </div>
-                <div>
+                <div className="bg-red-600">
                     <img></img>
-                    <h3>titulo 2</h3>
+                    <h3 className="text-black">titulo 2</h3>
                 </div>
-                <div>
+                <div className="bg-blue-600">
                     <img></img>
-                    <h3>titulo 3</h3>
+                    <h3 className="text-black">titulo 3</h3>
                 </div>
-                <div>
+                <div className="bg-green-800">
                     <img></img>
-                    <h3>titulo 4</h3>
+                    <h3 className="text-black">titulo 4</h3>
                 </div>
-                <button>izq</button>
-                <button>der</button>
+
             </div>
+            <button className="text-black">izq</button>
+            <button className="text-black">der</button>
         </section>
 
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,15 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import ShoppingCart from '@/components/SoppingCart/ShoppingCart'
+import Carousel from '@/components/carousel/Carousel'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main>
-     <ShoppingCart/>
+    <main className='bg-white min-h-screen '>
+     {/* <ShoppingCart/> */}
+     <Carousel/>
     </main>
   )
 }
