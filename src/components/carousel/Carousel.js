6532f5b89c0f74fd2a243ecb200a8b107f480d0e
Emanuel Pais <emapais228@gmail.com>
2023-08-31 13:58:04 -0300

    Creación componente

---
+++ src/components/carousel/Carousel.js
@@ -0,0 +1,39 @@
+
+import { useState } from "react"
+
+
+
+
+const Carousel = () => {
+
+
+
+    return (
+        <section>
+            <div>
+                <div>
+                    <img></img>
+                    <h3>titulo 1</h3>
+                </div>
+                <div>
+                    <img></img>
+                    <h3>titulo 2</h3>
+                </div>
+                <div>
+                    <img></img>
+                    <h3>titulo 3</h3>
+                </div>
+                <div>
+                    <img></img>
+                    <h3>titulo 4</h3>
+                </div>
+                <button>izq</button>
+                <button>der</button>
+            </div>
+        </section>
+
+
+    )
+}
+
+export default Carousel
