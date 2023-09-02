cc48fc4a994c3522a2f242be11ea333d0e093d49
Emanuel Pais <emapais228@gmail.com>
2023-09-02 13:57:59 -0300

    Posicionamiento de section
    Posicion de section para evitar conflicto con otros componentes

--- src/components/carousel/Carousel.js
+++ src/components/carousel/Carousel.js
@@ -39,30 +39,33 @@ const Carousel = () => {
 
     const handleNext = () => {
 
-     
-         setActiveIndex(activeIndex < items.length-1 ? activeIndex + 1: 0);
+
+        setActiveIndex(activeIndex < items.length - 1 ? activeIndex + 1 : 0);
 
 
         console.log(activeIndex)
     }
     const handlePrev = () => {
-        
 
-     
-        setActiveIndex(activeIndex> 0 ? activeIndex -1 : items.length-1)
+
+
+        setActiveIndex(activeIndex > 0 ? activeIndex - 1 : items.length - 1)
 
 
         console.log(activeIndex)
     }
 
     return (
-        <section className="pt-20 w-full  ">
-            <div className=" h-[400px] w-full relative">
-                <div style={{ background: `url(${items[activeIndex].image}) no-repeat center/cover` }} className="flex justify-between bg-black h-full w-[70%] items-center duration-500 relative m-auto rounded-lg  ">
-                    <h2 className="absolute bottom-0 pl-[10%] pb-7 text-xl">{items[activeIndex].text}</h2>
+        <section className="pt-20 pb-20 w-full ">
+            <div className=" h-[400px] w-full">
+                <div style={{
+                    background: `url(${items[activeIndex].image}) no-repeat 80% center/cover`,
+                }}
+                    className="flex justify-between bg-black h-full w-[90%] sm:w-[80%] xl:w-[70%] 2xl:w-[60%] items-center duration-500 relative m-auto rounded-lg  ">
+                    <a href="" className="absolute bottom-0 pl-[10%] pb-7 text-xl">{items[activeIndex].text}</a>
                     <div onClick={handlePrev} className=" ml-3 rounded-full bg-black/20 / text-xl  ">
 
-                        <FontAwesomeIcon className="cursor-pointer p-1" icon={faAnglesLeft}  size="xl" style={{ "--fa-primary-color": "#7f8b9f", "--fa-secondary-color": "#e70d0d", }} />
+                        <FontAwesomeIcon className="cursor-pointer p-1" icon={faAnglesLeft} size="xl" style={{ "--fa-primary-color": "#7f8b9f", "--fa-secondary-color": "#e70d0d", }} />
 
                     </div>
 
